e5013201650a57b3c9ffca202be04d8ce7c67352
sateras <satpekov02@mail.ru>
2023-03-14 12:27:50 +0600

    add file firebaseConfig

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { firebaseConfig } from "./utils/consts";
+import { firebaseConfig } from "./utils/firebaseConfig";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
