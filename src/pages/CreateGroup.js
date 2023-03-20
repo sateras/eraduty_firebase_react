7f97dbb3a63a3e7f49296dd9f7e1d2f0f4b70598
sateras <satpekov02@mail.ru>
2023-03-20 11:20:39 +0600

    add SearchUserById

--- src/pages/CreateGroup.js
+++ src/pages/CreateGroup.js
@@ -1,7 +1,5 @@
-import { collection } from "firebase/firestore";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore";
 import { AuthContext } from "..";
 import SearchIcon from "@mui/icons-material/Search";
 import {
@@ -15,40 +13,35 @@ import {
 } from "@mui/material";
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 import Group from "../components/group/Group";
-import { getAuth } from "firebase/auth";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function CreateGroup() {
   const { auth, firestore } = useContext(AuthContext);
-  const [user] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const [searchId, setSearchId] = useState("");
+  const [user, setUser] = useState({});
 
-  const [value, loading, error] = useCollection(
-    collection(firestore, "groups"),
-    {
-      snapshotListenOptions: { includeMetadataChanges: true },
+  const searchById = async () => {
+    try {
+      const usersRef = collection(firestore, "users");
+      const q = query(usersRef, where("uid", "==", searchId));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        setUser(doc.data());
+        console.log(doc.data());
+      });
+      // setLoading(true);
+      // const data = await getDocs(collection(firestore, "users"));
+      // data.docs.forEach((e) => {
+      //   if (e.data().uid === searchId) {
+      //   }
+      // });
+      // setLoading(false);
+    } catch (error) {
+      alert("Error: " + error);
     }
-  );
-
-  // getAuth()
-  //   .getUsers([
-  //     { uid: "uid1" },
-  //     { email: "user2@example.com" },
-  //     { phoneNumber: "+15555550003" },
-  //     { providerId: "google.com", providerUid: "google_uid4" },
-  //   ])
-  //   .then((getUsersResult) => {
-  //     console.log("Successfully fetched user data:");
-  //     getUsersResult.users.forEach((userRecord) => {
-  //       console.log(userRecord);
-  //     });
-
-  //     console.log("Unable to find users corresponding to these identifiers:");
-  //     getUsersResult.notFound.forEach((userIdentifier) => {
-  //       console.log(userIdentifier);
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error fetching user data:", error);
-  //   });
+  };
 
   return (
     <div style={{ overflowY: "auto", height: "calc(100vh - 70px)" }}>
@@ -62,24 +55,28 @@ function CreateGroup() {
       <div style={{ padding: 10, backgroundColor: "#e0e0e0" }}>
         Добавить участника
         <div style={{ display: "flex" }}>
-          <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Участник..." />
-          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <InputBase
+            value={searchId}
+            onChange={(e) => setSearchId(e.target.value)}
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Участник..."
+          />
+          <IconButton
+            onClick={searchById}
+            type="button"
+            sx={{ p: "10px" }}
+            aria-label="search"
+          >
             <SearchIcon />
           </IconButton>
         </div>
       </div>
       <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        <Group primary={"hellooo"} secondary={"wooorld"} />
-
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Summer BBQ"
-            secondary={"Wish I could come, but I'm out of town this…"}
-          />
-        </ListItem>
+        <Group
+          img={user.photoUrl}
+          primary={user.displayName}
+          secondary={user.uid}
+        />
       </List>
     </div>
   );
